ec77ad0f44521e50090a1277e7d7c2c004b9306f
Gat <ivanzzzo@gmail.com>
2023-09-16 15:59:01 +0300

    Разобрали цикл for

--- Example012_TypesOfMethods/Program.cs
+++ Example012_TypesOfMethods/Program.cs
@@ -34,18 +34,37 @@ int year = Method3();
 //Console.WriteLine(year);
 
 // Вид 4
+// string Method4(int count, string text)
+// {
+//     int i = 0;
+//     string result = String.Empty;
+
+//     while (i < count)
+//     {
+//         result = result + text;
+//         i++;
+//     }
+//     return result;
+
+// }
+
 string Method4(int count, string text)
 {
-    int i = 0;
     string result = String.Empty;
-
-    while (i < count)
+    for(int i = 0; i < count; i++)
     {
         result = result + text;
-        i++;
     }
     return result;
-
 }
 string res = Method4( 10, "asdf" );
-Console.WriteLine(res);
+//Console.WriteLine(res);
+
+for (int i = 2; i <= 10; i++)
+{
+    for (int j = 2; j <= 10; j++)
+    {
+        Console.WriteLine($"{i} x {j} = {i * j}");
+    }
+    Console.WriteLine();
+}

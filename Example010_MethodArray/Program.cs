a1a3d9efef7bd2913a0103df44e5d2f5ceb0d2f1
Gat <ivanzzzo@gmail.com>
2023-09-16 12:17:43 +0300

    Поиск нужного числа в массиве

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = { 3, 43, 54, 664, 66, 22, 778, 9 };
+
+int n = array.Length;
+int find = 22;
+
+int index = 0;
+
+while (index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        //break; если несколько значений
+    }
+    index++;
+}
